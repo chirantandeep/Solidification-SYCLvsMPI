c635792050c978c9465fe41b68c46da86d45f1a4
Chirantandip Mahanta <chirantandipmahanta@gmail.com>
2023-07-17 15:11:47 +0530

    Delete KOBPFM directory

--- KOBPFM/src/compile_time_vars.h
+++
@@ -1,9 +0,0 @@
-#ifndef compiletimevars
-#define compiletimevars
-
-namespace CT_VARS{
-int constexpr DIM = KOB_SYSTEM_DIMENTIONS ;
-int constexpr STENCIL_SIZE = 9 ;
-int constexpr MAX_BARRIER_POTENTIAL_TERMS = 9 ;
-}
-#endif
